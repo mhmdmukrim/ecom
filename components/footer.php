9901140642f98b0e3699814c71a7c55b0a16ea24
DEVIL-ALPHA <92577442+mhmdmukrim@users.noreply.github.com>
2024-02-18 14:50:13 +0530

    mukrim

---
+++ components/footer.php
@@ -0,0 +1,42 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+         <a href="admin/admin_login.php"><i class="fas fa-angle-right"></i> admin</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="https://wa.me/94755545015"><i class="fas fa-phone"></i> +94 75 5545 015</a>
+         <a href="tel:94766395983"><i class="fas fa-phone"></i> +94 76 6395 983</a>
+         <a href="mailto:mukrimmhmd@gmail.com"><i class="fas fa-envelope"></i> mukrimmhmd@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Kandy, Sri Lanka - 20000 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>MHMD MUKRIM</span> | all rights reserved!</div>
+
+</footer>
